66c9d75abe495da381a5cc52f4eb6242dff390e8
Nemiseticus <Nemiseticus>
2021-05-11 00:18:24 -0300

    Implementando put-ser

--- blog-Pessoal/src/app/app-routing.module.ts
+++ blog-Pessoal/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { TemaDitComponent } from './edit/tema-dit/tema-dit.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
@@ -24,7 +25,8 @@ const routes: Routes = [
 {path: 'tema-edit/:id', component: TemaDitComponent},
 {path: 'tema-delete/:id', component: TemaDeleteComponent},
 {path: 'postagem-edit/:id', component: PostagemEditComponent},
-{path: 'postagem-delete/:id', component: PostagemDeleteComponent}
+{path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+{path: 'user-edit/:id', component: UserEditComponent}
 ];
 
 @NgModule({
--- blog-Pessoal/src/app/app.module.ts
+++ blog-Pessoal/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { TemaDitComponent } from './edit/tema-dit/tema-dit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     TemaDitComponent,
     TemaDeleteComponent,
     PostagemEditComponent,
-    PostagemDeleteComponent
+    PostagemDeleteComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
